94685c2bbe5de11c6fc6dd9c410c545d9577f891
SashaSe <sashasemenchuk21@gmail.com>
2022-06-29 23:52:05 +0300

    React

---
+++ React/first-app/react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+      <div className="App">
+        <ul>
+          <li>
+          </li>
+        </ul>
+      </div>
+  );
+}
+
+export default App;
