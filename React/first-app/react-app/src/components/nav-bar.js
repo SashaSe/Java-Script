446588231f4d43658cee0e2c548774e910c6c273
SashaSe <sashasemenchuk21@gmail.com>
2022-07-01 23:50:04 +0300

    React

---
+++ React/first-app/react-app/src/components/nav-bar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav className="nav-bar">
+            <div>
+                <a href="#">Profile</a>
+            </div>
+            <div>
+                <a href="#">Messages</a>
+            </div>
+            <div>
+                <a href="#">News</a>
+            </div>
+            <div>
+                <a href="#">Music</a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
